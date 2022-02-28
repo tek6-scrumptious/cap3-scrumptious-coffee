4ee7b7b1cc1bc1b7f0f8a18e6ef4f4de97975939
AlanSobenes <alansobenes@gmail.com>
2022-02-28 12:19:41 -0800

    landing page image, structure and styling; complete

--- scrumptious-coffee/src/pages/landing/Landing.jsx
+++ scrumptious-coffee/src/pages/landing/Landing.jsx
@@ -6,7 +6,7 @@ export default function Landing() {
 
   return (
     <div>
-      <h1 className='name' >Scrumptious</h1>
+      <h1 className='name'> <a href="/" className="link landingHover">Scrumptious </a> </h1>
       <img src={process.env.PUBLIC_URL + '/images/landingPage.jpg'} className='image' alt="Two coffees with plants" />
     </div>
   );
