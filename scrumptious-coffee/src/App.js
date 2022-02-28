dd78690857ee9585e36520332c5e6e879846b70a
Typical-Joey <jtcapocci@yahoo.com>
2022-02-28 12:45:48 -0600

    Add footer

--- scrumptious-coffee/src/App.js
+++ scrumptious-coffee/src/App.js
@@ -5,12 +5,14 @@ import Landing from "./pages/landing/Landing";
 // styles
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Landing />
+      <Footer />
     </div>
   );
 }
--- scrumptious-coffee/src/components/footer/Footer.jsx
+++ scrumptious-coffee/src/components/footer/Footer.jsx
@@ -1,10 +1,23 @@
+import { Col, Container, Nav, Row } from "react-bootstrap";
 // styles
 import "./Footer.css";
 
 export default function Footer() {
+  // React Router needs to be installed and links need to be styled
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <>
+      <Container fluid className="footer">
+        <Row>
+          <Col>
+            <a href="/">About Us</a>
+          </Col>
+          <Col>
+            <a href="/">Return Policy</a>
+          </Col>
+          <Col>©Scrumptious: 2022</Col>
+          <Col>Date Established: 2/28/2022</Col>
+        </Row>
+      </Container>
+    </>
   );
 }
