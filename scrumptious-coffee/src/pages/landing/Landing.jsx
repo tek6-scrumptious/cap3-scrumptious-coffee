ae5c8429505f0467c34b6cef2a0ad71dc10267f1
AlanSobenes <alansobenes@gmail.com>
2022-02-28 12:05:29 -0800

    Co-authored-by: raboomar <raboomar@users.noreply.github.com> Co-authored-by: Michael <michaellynn-r@users.noreply.github.com>

--- scrumptious-coffee/src/pages/landing/Landing.jsx
+++ scrumptious-coffee/src/pages/landing/Landing.jsx
@@ -1,10 +1,13 @@
 // styles
 import "./Landing.css";
 
+
 export default function Landing() {
+
   return (
-    <div className="height">
-      <h1>Landing</h1>
+    <div>
+      <h1 className='name' >Scrumptious</h1>
+      <img src={process.env.PUBLIC_URL + '/images/landingPage.jpg'} className='image' alt="Two coffees with plants" />
     </div>
   );
 }
