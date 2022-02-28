70cdb1a6e23c992a882d7667c1c71b225b388513
Typical-Joey <jtcapocci@yahoo.com>
2022-02-28 12:19:18 -0600

    Structured Navbar

--- scrumptious-coffee/src/App.js
+++ scrumptious-coffee/src/App.js
@@ -1,9 +1,16 @@
+// pages and components
+import Header from "./components/header/Header";
+import Landing from "./pages/landing/Landing";
+
+// styles
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Home</h1>
+      <Header />
+      <Landing />
     </div>
   );
 }
