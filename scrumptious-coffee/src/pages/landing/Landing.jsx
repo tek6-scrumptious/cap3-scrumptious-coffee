0c03818fbd3c4446d229822821d5d65091c7ce7b
AlanSobenes <alansobenes@gmail.com>
2022-03-02 11:25:46 -0800

    changed landing page orientation for mobile, and added logo to mobile and desktop versions with a shrink hover effect -AS

--- scrumptious-coffee/src/pages/landing/Landing.jsx
+++ scrumptious-coffee/src/pages/landing/Landing.jsx
@@ -5,17 +5,40 @@ import "./Landing.css";
 export default function Landing() {
   return (
     <div>
-      <h1 className="name">
-        {" "}
-        <Link to="/products" className="links landingHover">
-          Scrumptious{" "}
-        </Link>{" "}
-      </h1>
-      <img
-        src={process.env.PUBLIC_URL + "/images/landingPage.jpg"}
-        className="image"
-        alt="Two coffees with plants"
-      />
+      {window.innerWidth <= 415 ? (
+        <div>
+          <div>
+            <Link to="/products">
+              <img
+                src={process.env.PUBLIC_URL + "/images/ScrumptiousLogo.png"}
+                className="logoMobile"
+                alt="scrumptious logo"
+              />
+            </Link>
+            <img
+              src={process.env.PUBLIC_URL + "/images/landingPageMobile.jpg"}
+              className="image"
+              alt="Two coffees with plants"
+            />
+          </div>
+        </div>) : (
+          <div>
+          <div>
+            <Link to="/products">
+              <img
+                src={process.env.PUBLIC_URL + "/images/ScrumptiousLogo.png"}
+                className="logo shrink"
+                alt="scrumptious logo"
+              />
+            </Link>
+            <img
+              src={process.env.PUBLIC_URL + "/images/landingPage.jpg"}
+              className="image"
+              alt="Two coffees with plants"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
