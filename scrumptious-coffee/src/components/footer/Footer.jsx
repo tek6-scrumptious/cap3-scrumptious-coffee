fe77d00b757b4d691cb007a00c68d4ee4375f037
dinahbrito <dinahbrito@gmail.com>
2022-03-02 11:03:23 -0800

    mobile changes updated according to meeting

--- scrumptious-coffee/src/components/footer/Footer.jsx
+++ scrumptious-coffee/src/components/footer/Footer.jsx
@@ -3,20 +3,23 @@ import { Link } from "react-router-dom";
 // styles
 import "./Footer.css";
 
-export default function Footer() {
+export default function Footer()
+{
   // React Router needs to be installed and links need to be styled
   return (
     <>
       <Container fluid className="footer">
-        <Row>
+        <Row className="footer-row">
+          <Col>Est: 2/28/2022</Col>
+
           <Col>
             <Link className='links' to="/aboutus">About Us</Link>
           </Col>
           <Col>
             <Link className='links' to="/return_policy">Return Policy</Link>
           </Col>
-          <Col>©Scrumptious: 2022</Col>
-          <Col>Date Established: 2/28/2022</Col>
+          <Col>© Scrumptious 2022</Col>
+
         </Row>
       </Container>
     </>
