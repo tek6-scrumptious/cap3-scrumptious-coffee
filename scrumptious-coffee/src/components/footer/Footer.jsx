dd2cf5ea6eff5a6fd532e9cd73abc3573515844a
dinahbrito <dinahbrito@gmail.com>
2022-03-02 11:48:18 -0800

    removed old comments from footer

--- scrumptious-coffee/src/components/footer/Footer.jsx
+++ scrumptious-coffee/src/components/footer/Footer.jsx
@@ -5,13 +5,11 @@ import "./Footer.css";
 
 export default function Footer()
 {
-  // React Router needs to be installed and links need to be styled
   return (
     <>
       <Container fluid className="footer">
         <Row className="footer-row">
           <Col>Est: 2/28/2022</Col>
-
           <Col>
             <Link className='links' to="/aboutus">About Us</Link>
           </Col>
@@ -19,7 +17,6 @@ export default function Footer()
             <Link className='links' to="/return_policy">Return Policy</Link>
           </Col>
           <Col>© Scrumptious 2022</Col>
-
         </Row>
       </Container>
     </>
