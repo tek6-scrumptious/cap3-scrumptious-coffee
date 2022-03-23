bc682e91c49785c7bb773fd7c65ecb924785be53
AlanSobenes <alansobenes@gmail.com>
2022-03-23 10:44:38 -0700

    404 page is ready, with css and responsiveness

--- scrumptious-coffee/src/pages/Error/Error.jsx
+++ scrumptious-coffee/src/pages/Error/Error.jsx
@@ -4,18 +4,20 @@ import "./Error.css";
 
 const Error = () => {
   return <div>
-    
+       <div className="error-Text">
+    <h1>404 Error</h1>
+    <h3>Oops, looks like we spilled the beans here.
+    <br /> 
+    Please go back and try again.</h3>
+    </div>
 
     <img 
       src={process.env.PUBLIC_URL + "/images/Error404SpilledBeansImage.png"} 
-      className="image"
+      className="error-Image"
       alt="Spilled coffee image"
     />
+ 
 
-    <h1>404 Error</h1>
-    <h3>Oops, looks like we spilled the beans here.
-      <br /> 
-      Please go back and try again.</h3>
     
     </div>;
 };
