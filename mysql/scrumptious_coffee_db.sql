95c6953926b62df61dc177884af3f076799e6ce5
Rami <aboomar.2@osu.edu>
2022-04-05 14:18:12 -0400

    Created database

---
+++ mysql/scrumptious_coffee_db.sql
@@ -0,0 +1,38 @@
+-- CREATE DATABASE scrumptious_coffee_db;
+
+-- use scrumptious_coffee_db;
+
+-- DROP TABLE products;
+-- CREATE TABLE products(
+-- products_id int AUTO_INCREMENT PRIMARY KEY,
+-- name varchar(255) not null,
+-- image_url varchar(255) not null, 
+-- description varchar(255) not null,
+-- store_quanity int not null, 
+-- price_per_pound decimal(4,2)
+
+-- );
+
+-- INSERT INTO products(name,image_url,description,store_quanity,price_per_pound)
+-- VALUES
+-- ('Hawaiian Kona Coffee','https://imgur.com/RiHhhZi','Well-balanced with a medium body, it is clean in the cup with a bright and cheerful acidity. Kona coffee often reveals buttery as well as spicy qualities and subtle winey tones with an excellent aromatic finish',
+-- 12,12.99),
+-- ('Mocha Java Coffee','https://imgur.com/RHm8Tck','Perhaps the most famous blend of coffee beans, Mocha Java includes Arabian (Yemen) Mocha coffee and Indonesian Java Arabica coffee, two coffees with complementary characteristics.',
+-- 15,12.77),
+-- ('Kenya AA Coffee','https://imgur.com/yokkvP1',"The best Kenya AA coffees exhibit a full body and strong, rich taste with a pleasant acidity that some say provides the world's brightest coffee.",
+-- 15,13.33),
+-- ("Tanzania Peaberry Coffee","https://imgur.com/qxRcvQ7","A medium roast provides an aroma that is floral and complex, often exhibiting hints of pineapple, citrus, or coconut. The flavor is delicate!",
+-- 12,11.70),
+-- ("Nicaraguan Coffee","https://imgur.com/oZdExjf","Darker roasts bring compliment the chocolate and fruity flavors.",12,21.35),
+-- ("Sumatra Mandheling Beans","https://imgur.com/P6EOlfr","The caramelizing that happens in a dark roast helps to mediate the earthy, herby flavor.",8,14.56),
+-- ("Sulawesi Toraja Coffee","https://imgur.com/ESDYf50","This multi-dimensional coffee is grown in the southeastern highlands of Sulawesi. Known best for its full body and rich, expansive flavor, Sulawesi Toraja coffee beans are very well balanced and exhibits tasting notes of dark chocolate and ripe fruit.",
+-- 20,15.71),
+-- ("Ethiopian Harrar Coffee","https://imgur.com/LeD8KOU","Edgy and bold, Ethiopian Harrar displays a complexity of spice tones including cardamom, cinnamon, apricots, blueberry jam, and compote. Some Harrars exhibit tones of very rich, dark chocolate.",
+-- 15,13.04),
+-- ("Ethiopian Yirgacheffe Coffee","https://imgur.com/AUD1AHR","If you prefer your coffee heavy and sweet then choose a medium-dark roast or dark roast, though a medium roast allows the coffee beans' delicate qualities to shine and enhance the bright acidity.",
+-- 15,16.46),
+-- ("Guatemala Antigua Coffee","https://imgur.com/7tkdZxw","Guatemalan coffees are naturally smooth and delicious, and works well with a dark roast that creates a pleasing smoky taste in the brewed cup of coffee.",
+-- 10,13.56)
+-- ;
+
+-- SELECT * FROM products;
