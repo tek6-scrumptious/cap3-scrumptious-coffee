fb00141be8e476ded981a99b9f8857650d0850c8
AlanSobenes <alansobenes@gmail.com>
2022-04-09 11:29:22 -0700

    pushing route to admin-dashboard

--- scrumptious-coffee/src/pages/adminLogin/AdminLogin.jsx
+++ scrumptious-coffee/src/pages/adminLogin/AdminLogin.jsx
@@ -1,27 +1,53 @@
-import { React } from "react";
+import axios from "axios";
+import { React, useState } from "react";
 import { Form, Button } from "react-bootstrap"
+import {  useNavigate } from "react-router-dom";
 import "./AdminLogin.css"
 
 const AdminLogin = () => {
+    const navigate = useNavigate();
+    const[userName ,setUserName]=useState("Alan");
+    const[password, setPassword]=useState("tekcamp");
 
+
+    const submit = (e) => {
+        e.preventDefault();
+        console.log("here")
+        const login = {
+            username: userName,
+            password: password
+        }
+        // axios.post('http://localhost:8080/admin/login',{
+        //     userName,
+        //     password
+        // } )
+        // .then(res=>{
+            navigate('/admin-dashboard')
+        // })
+        // .catch(err => console.log(err))
+
+    }
     return (
-        <div className="adminlogin-structure">
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="email" placeholder="Enter username" />
+        <div className="overall-structure">
+            <div className="adminlogin-structure">
+                <Form onSubmit={submit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control  placeholder="Enter username" />
 
-                </Form.Group>
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    {/* <a className="bn39" style={{ marginRight: '20px' }} onClick={(e) => submit(e)}><span className="bn39span"><h6>Add to Cart</h6></span></a> */}
+                    <Button variant="primary" type="submit" >
+                        Login
+                    </Button>
+                </Form>
+            </div>
 
-                <Button variant="primary" type="submit" >
-                    Login
-                </Button>
-            </Form>
         </div>
 
     );
