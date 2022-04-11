f1cd2e962553f8404cc57d2cab16a82eb359a252
Typical-Joey <jtcapocci@yahoo.com>
2022-04-11 17:06:27 -0500

    Change profile images, and added some team member pictures

--- scrumptious-coffee/src/pages/aboutUs/AboutUs.jsx
+++ scrumptious-coffee/src/pages/aboutUs/AboutUs.jsx
@@ -3,86 +3,129 @@ import "./AboutUs.css";
 import { Card, Button, Container } from "react-bootstrap";
 import PersonalCard from "./PersonalCard";
 import { Link } from "react-router-dom";
-// import CoffeeImages from ""
 
 export default function AboutUs() {
   return (
-    <Container className="about-us-container">
-      <Card>
-        <Card.Body>
-          <Card.Img
-            variant="top"
-            src={process.env.PUBLIC_URL + "images/Global-Map-With-Beans.png"}
-          />
-          <hr />
-          <Card.Text>
-            <Card.Title>From Mountaintop to countertop</Card.Title>
-            Our coffee master, Jon Kol, only selects the top 1% of Arabica beans
-            from the world’s best growing regions in East Africa, Latin America,
-            and the Pacific. <br /> <br />
-            We roast in small batches at our facility in Camarillo, CA, where we
-            find the roast that best suits the beans from each origin and
-            captures what makes each country’s coffee unique. In other words, we
-            don’t do anything halfway.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <div className="about-us-margin-top about-us-card">
-        <h3>Our Journey</h3>
-        <hr />
+    <>
+      <Container className="about-us-container">
         <Card>
           <Card.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna
-            cursus eget nunc scelerisque viverra mauris. Rhoncus est
-            pellentesque elit ullamcorper.
+            <Card.Img
+              variant="top"
+              src={process.env.PUBLIC_URL + "images/Global-Map-With-Beans.png"}
+            />
+            <hr />
+            <Card.Title>From Mountaintop to countertop</Card.Title>
+            <Card.Text>
+              Our coffee master, Jon Kol, only selects the top 1% of Arabica
+              beans from the world’s best growing regions in East Africa, Latin
+              America, and the Pacific. <br /> <br />
+              We roast in small batches at our facility in Camarillo, CA, where
+              we find the roast that best suits the beans from each origin and
+              captures what makes each country’s coffee unique. In other words,
+              we don’t do anything halfway.
+            </Card.Text>
           </Card.Body>
         </Card>
+      </Container>
+      <Container>
+        <div className="about-us-margin-top">
+          <h3>Our Journey</h3>
+          <hr />
+          <Card>
+            <Card.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna
+              cursus eget nunc scelerisque viverra mauris. Rhoncus est
+              pellentesque elit ullamcorper.
+            </Card.Body>
+          </Card>
 
-        <h3 className="about-us-margin-top">Our Team:</h3>
-        <hr />
-        <PersonalCard
-          title="Jon Kol (Coffee Master)"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
-        />
-        <PersonalCard
-          position="about-us-image-right"
-          title="Alan Sobenes"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
-        />
-        <PersonalCard
-          title="Dinah Brito"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
-        />
-        <PersonalCard
-          title="Jonothan Castillo"
-          position="about-us-image-right"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
-        />
-        <PersonalCard
-          title="Joseph Capocci"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
-        />
-        <PersonalCard
-          title="Michael Rivera"
-          position="about-us-image-right"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
-        />
-        <PersonalCard
-          title="Rami Aboomar"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
-        />
-        <PersonalCard
-          title="Waleed Saleh"
-          position="about-us-image-right"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
-        />
-        <Button size="lg" variant="success" className="about-us-button">
-          <Link to="/products" className="link">
-            CHECK OUT OUR COFFEE
-          </Link>
-        </Button>
-      </div>
-    </Container>
+          <h3 className="about-us-margin-top">Our Team:</h3>
+          <hr />
+          <div className="about-us-cards-container">
+            <div className="personal-card-left">
+              <PersonalCard
+                title="Jon Kol (Coffee Master)"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
+                other="about-us-cards-container-left about-us-grid-left"
+                imageUrl
+              />
+            </div>
+            <div className="personal-card-right">
+              <PersonalCard
+                title="Alan Sobenes"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
+                position="about-us-image-right"
+                other="about-us-cards-container-right"
+                bodyPosition="about-us-grid-right"
+                imageUrl
+              />
+            </div>
+            <div className="personal-card-left">
+              <PersonalCard
+                title="Dinah Brito"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
+                other="about-us-cards-container-left"
+                imageUrl="dinah"
+              />
+            </div>
+            <div className="personal-card-right">
+              <PersonalCard
+                title="Jonothan Castillo"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
+                position="about-us-image-right"
+                other="about-us-cards-container-right"
+                bodyPosition="about-us-grid-right"
+                imageUrl="jonothan"
+              />
+            </div>
+            <div className="personal-card-left">
+              <PersonalCard
+                title="Joseph Capocci"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
+                other="about-us-cards-container-left"
+                imageUrl="joey"
+              />
+            </div>
+            <div className="personal-card-right">
+              <PersonalCard
+                title="Michael Rivera"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
+                position="about-us-image-right"
+                other="about-us-cards-container-right"
+                bodyPosition="about-us-grid-right"
+                imageUrl="michael"
+              />
+            </div>
+            <div className="personal-card-left">
+              <PersonalCard
+                title="Rami Aboomar"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
+                other="about-us-cards-container-left"
+                imageUrl="rami"
+              />
+            </div>
+            <div className="personal-card-right">
+              <PersonalCard
+                title="Waleed Saleh"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra mauris. Rhoncus est pellentesque elit ullamcorper."
+                position="about-us-image-right"
+                other="about-us-cards-container-right"
+                bodyPosition="about-us-grid-right"
+                imageUrl
+              />
+            </div>
+          </div>
+          <div className="about-us-button-container">
+            <Button size="lg" variant="success" className="about-us-button">
+              <Link to="/products" className="link">
+                CHECK OUT OUR COFFEE
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
--- scrumptious-coffee/src/pages/aboutUs/PersonalCard.jsx
+++ scrumptious-coffee/src/pages/aboutUs/PersonalCard.jsx
@@ -5,12 +5,17 @@ import { Card } from "react-bootstrap";
 export default function PersonalCard(props) {
   return (
     <>
-      <Card className="about-us-margin-top about-us-card">
+      <Card className={"about-us-margin-top about-us-card " + props.other}>
         <Card.Header as="h5">{props.title}</Card.Header>
-        <Card.Body className="about-us-grid">
+        <Card.Body className={"about-us-grid " + props.bodyPosition}>
           <img
             variant="top"
-            src={process.env.PUBLIC_URL + "images/ScrumptiousLogo.png"}
+            src={
+              process.env.PUBLIC_URL +
+              "images/about-us-images/" +
+              props.imageUrl +
+              ".png"
+            }
             alt=""
             className={"about-us-profile-image " + props.position}
           />
