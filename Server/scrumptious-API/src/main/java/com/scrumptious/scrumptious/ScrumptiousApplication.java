bf7598aa9143a9ee74a68017009c09c4c3bb17fd
Rami <aboomar.2@osu.edu>
2022-04-11 15:42:48 -0400

    fixed get route to deploy

--- Server/scrumptious-API/src/main/java/com/scrumptious/scrumptious/ScrumptiousApplication.java
+++ Server/scrumptious-API/src/main/java/com/scrumptious/scrumptious/ScrumptiousApplication.java
@@ -2,9 +2,10 @@ package com.scrumptious.scrumptious;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScrumptiousApplication {
+public class ScrumptiousApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrumptiousApplication.class, args);
