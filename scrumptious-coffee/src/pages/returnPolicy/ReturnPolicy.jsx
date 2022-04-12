ce12b42e7674953c65f3cdd279f3edf8a6fabba9
Jonothan <jonothan.x.castillo@gmail.com>
2022-04-12 11:59:14 -0500

    Removed the Q from the faq questions

--- scrumptious-coffee/src/pages/returnPolicy/ReturnPolicy.jsx
+++ scrumptious-coffee/src/pages/returnPolicy/ReturnPolicy.jsx
@@ -20,7 +20,7 @@ export default function ReturnPolicy() {
 
       <Accordion className="faq-accordion-container">
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Q1. What are my Payment Options?</Accordion.Header>
+          <Accordion.Header>What are my Payment Options?</Accordion.Header>
           <AccordionBody>
             We currently accept online orders through our website. We accept all major credit cards including Visa, MasterCard, and American Express.
             Please feel free to contact us with any questions regarding payments.
@@ -28,7 +28,7 @@ export default function ReturnPolicy() {
           </AccordionBody>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Q2. What are my Shipping Options?</Accordion.Header>
+          <Accordion.Header>What are my Shipping Options?</Accordion.Header>
           <AccordionBody>
             We currrently ship to the contiguous 48 US States, Hawaii, Alaska, US Territories, and APO/FPO/DPO.
             <br />
@@ -37,48 +37,13 @@ export default function ReturnPolicy() {
           </AccordionBody>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
-          <Accordion.Header>Q3. What is the Return Policy?</Accordion.Header>
+          <Accordion.Header>What is the Return Policy?</Accordion.Header>
           <AccordionBody>
             We accept returns within 2 weeks from delivery of your order. To start a return, please contact us at returns@scrumptiouscoffee.com.
             <br />
           </AccordionBody>
         </Accordion.Item>
       </Accordion>
-
-      {/* <Card style={{width:'825px'}}>
-          <Card.Body>
-            <Card.Title>Payment Options</Card.Title>
-            <Card.Text >
-             We currently accept online orders through our website. We accept all major credit cards including Visa, MasterCard, and American Express.
-              <br />
-              Please feel free to contact us with any questions regarding payments.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <br />
-        <Card  style={{width:'825px'}}>
-          <Card.Body>
-            <Card.Title>Shipping Options</Card.Title>
-            <Card.Text >
-              We currrently ship to the contiguous 48 US States, Hawaii, Alaska, US Territories, and APO/FPO/DPO. 
-                <br />
-              We offer standard shipping on all orders. We ship orders Monday-Friday. All orders placed after Friday 12pm CST will be shipped the following Monday. 
-              <br />
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <br />
-        <Card  style={{width:'825px'}}>
-          <Card.Body>
-            <Card.Title>Return Policy</Card.Title>
-            <Card.Text >
-              We accept returns within 2 weeks from delivery of your order. To start a return, please contact us at returns@scrumptiouscoffee.com.
-              <br />
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <br /> */}
-
     </div>
   );
 
