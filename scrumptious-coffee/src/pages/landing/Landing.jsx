cb2c2ebf1d15ed32c2d4d226699ffd566cd6f49f
dinahbrito <dinahbrito@gmail.com>
2022-04-13 11:56:46 -0700

    made bg img responsive w/ external css

--- scrumptious-coffee/src/pages/landing/Landing.jsx
+++ scrumptious-coffee/src/pages/landing/Landing.jsx
@@ -4,7 +4,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Landing.css";
 
-export default function Landing() {
+export default function Landing()
+{
   return (
     <div className="landing-container">
       {window.innerWidth <= 415 ? (
@@ -18,7 +19,7 @@ export default function Landing() {
               />
             </Link>
             <img
-              src={process.env.PUBLIC_URL + "/images/landingPageMobile3.png"}
+              // src={process.env.PUBLIC_URL + "/images/landingPageMobile3.png"}
               className="imageMobile"
               alt="Two coffees with plants"
             />
