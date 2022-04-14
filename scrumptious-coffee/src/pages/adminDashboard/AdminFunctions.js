bb6f3644dd4522531020aa682df19bd17244cbbe
Rami <aboomar.2@osu.edu>
2022-04-14 19:58:20 -0400

    allowed admin to select location and roast type

--- scrumptious-coffee/src/pages/adminDashboard/AdminFunctions.js
+++ scrumptious-coffee/src/pages/adminDashboard/AdminFunctions.js
@@ -7,6 +7,8 @@ export const formValidation = (
   quantity,
   description,
   imageUrl,
+  roast,
+  location,
   inputs
 ) => {
   let flag = 5;
@@ -30,8 +32,8 @@ export const formValidation = (
       description: description,
       storeQuantity: quantity,
       pricePrePound: price,
-      roast: "fff",
-      location: "fff",
+      roast: roast,
+      location: location,
     };
     addNewProduct(jsonData);
   }
