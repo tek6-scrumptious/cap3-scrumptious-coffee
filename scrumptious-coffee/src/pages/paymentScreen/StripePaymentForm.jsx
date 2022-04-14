e4b2356bb96790a64e7fad273180d9cc184e31d3
Typical-Joey <jtcapocci@yahoo.com>
2022-04-13 23:18:15 -0500

    Add cart amount to stripe

--- scrumptious-coffee/src/pages/paymentScreen/StripePaymentForm.jsx
+++ scrumptious-coffee/src/pages/paymentScreen/StripePaymentForm.jsx
@@ -2,20 +2,22 @@ import StripeCheckout from "react-stripe-checkout";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const StripePaymentForm = () => {
+const StripePaymentForm = (props) => {
   const stripeKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
 
   const navigate = useNavigate();
+
   function handleToken(token, shipping) {
     const url =
       "http://scrumptious-env-2.eba-ixgv7adq.us-east-1.elasticbeanstalk.com/create-payment-intent";
     axios
       .post(url, {
         token: token.id,
-        amount: 500 * 100, // come from the cart redux state
+        amount: props.amount * 100, 
       })
       .then((response) => {
         if (response.status === 200) {
+          // Clear cart
           navigate("/order-confirmation");
         }
       })
@@ -24,17 +26,17 @@ const StripePaymentForm = () => {
   }
 
   return (
-    <div>
+    <>
       <StripeCheckout
         stripeKey={stripeKey}
         token={handleToken}
         autocomplete="off"
         billingAddress
         shippingAddress
-        amount={2.0 * 100}
+        amount={props.amount * 100}
         name={"Scrumptious Coffee"}
       />
-    </div>
+    </>
   );
 };
 export default StripePaymentForm;
