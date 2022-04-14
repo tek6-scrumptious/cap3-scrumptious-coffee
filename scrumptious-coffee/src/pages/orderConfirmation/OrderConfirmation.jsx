d96a606c76cf96d0838f3c435ae7d6963d795c49
Typical-Joey <jtcapocci@yahoo.com>
2022-04-14 14:07:59 -0500

    Clear cart and order on refresh -WS, -JC

--- scrumptious-coffee/src/pages/orderConfirmation/OrderConfirmation.jsx
+++ scrumptious-coffee/src/pages/orderConfirmation/OrderConfirmation.jsx
@@ -1,104 +1,154 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 // styles
 import "./OrderConfirmation.css";
-import { Image, CardGroup, Card } from "react-bootstrap";
+import { Image, CardGroup, Card, ListGroup, Row, Col } from "react-bootstrap";
 
 export default function OrderConfirmation() {
-  const orders = JSON.stringify(localStorage.getItem("order"));
-  console.log(orders);
+  const orders = JSON.parse(localStorage.getItem("order"));
+
+  const [subTotal, setSubTotal] = useState(0);
+  const [tax, setTax] = useState(0);
+
+  const cart = useSelector((state) => state.cart);
+  const cartItems = cart.inCart;
+
+  function getDate() {
+    const date = new Date().toUTCString();
+    return date;
+  }
+
+  const subTotalMethod = useCallback(() => {
+    let addSubTotal = 0;
+    cartItems.forEach((item) => {
+      addSubTotal = addSubTotal += item.pricePrePound * item.qty;
+    });
+    setSubTotal(addSubTotal.toFixed(2));
+  }, [cartItems]);
+
+  const taxMethod = useCallback(() => {
+    let addTax = 0;
+    addTax = subTotal * 0.08;
+    setTax(addTax.toFixed(2));
+  }, [subTotal]);
+
+  useEffect(() => {
+    subTotalMethod();
+    taxMethod();
+    window.onunload = () => {
+      localStorage.removeItem("cartItems");
+      localStorage.removeItem("order");
+    };
+  }, [subTotalMethod, taxMethod]);
+
   return (
-    <div className="confirmation-container">
-      <div className="p-5 text-center bg-light">
-        <h1 className="mb-3">Thank you for your order!</h1>
-        <h4 className="mb-3">Order number: </h4>
-        <h6 className="mb-3">Date/Time:</h6>
-      </div>
+    <>
+      {!window.localStorage.getItem("order") ? (
+        <div className="order-confirmation-jumbotron">
+          <h1>No order has been made</h1>
+        </div>
+      ) : (
+        <div className="confirmation-container">
+          <div className="p-5 text-center bg-light">
+            <h1 className="mb-3">Thank you for your order!</h1>
+            <h4 className="mb-3">
+              Order number: {orders.id.replace("tok_", "")}
+            </h4>
+            <h6 className="mb-3">Date/Time: {getDate()}</h6>
+          </div>
 
-      <CardGroup>
-        <Card>
-          <Card.Body>
-            <Card.Title>Shipping Information</Card.Title>
-            <Card.Text className="order-confirmation-indent">
-              Name:
-              <br />
-              Address:
-              <br />
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>Billing</Card.Title>
-            <Card.Text className="order-confirmation-indent">
-              Name:
-              <br />
-              Address:
-              <br />
-              Card Number Ending in :
-              <br />
-              Email:
-              <br />
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>Shipping Method</Card.Title>
-            <Card.Text className="order-confirmation-indent">
-              <br />
-              SHIPPING TYPE: UPS GROUND 7-8 business days
-              <br />
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </CardGroup>
-
-      <CardGroup className="order-confirmation-summary">
-        <Card>
-          <Card.Body>
-            <Card.Title>Order List: </Card.Title>
+          <CardGroup>
             <Card>
-              <div className="order-confirmation-item">
-                <Image
-                  className="order-confirmation-thumbnail"
-                  src="https://i.ebayimg.com/images/g/leIAAOSwg19h-p7v/s-l1600.jpg"
-                />
-
-                <div>
-                  <Card.Title>Kona Coffee</Card.Title>
-                  <Card.Text>
-                    Weight:
-                    <br />
-                    Price:
-                    <br />
-                    Quantity:
-                    <br />
-                    Item Total:
-                    <br />
-                  </Card.Text>
-                </div>
-                <button className="delete-item-button"> X </button>
-              </div>
+              <Card.Body>
+                <Card.Title>Shipping Information</Card.Title>
+                <Card.Text className="order-confirmation-indent">
+                  <strong>Name</strong>: {orders.shipName}
+                  <br />
+                  <strong>Address</strong>: {orders.shipAddress}
+                  <br />
+                </Card.Text>
+              </Card.Body>
             </Card>
-          </Card.Body>
-        </Card>
+            <Card>
+              <Card.Body>
+                <Card.Title>Billing</Card.Title>
+                <Card.Text className="order-confirmation-indent">
+                  <strong>Name</strong>: {orders.billName}
+                  <br />
+                  <strong>Address</strong>: {orders.billAddress}
+                  <br />
+                  <strong>Card Number Ending in</strong>: {orders.last4}
+                  <br />
+                  <strong>Email</strong>: {orders.email}
+                  <br />
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </CardGroup>
 
-        <Card>
-          <Card.Body>
-            <Card.Title>Order Summary </Card.Title>
-            <Card.Text className="order-confirmation-indent">
-              Subtotal: $15.00
-              <br />
-              Tax: $0.89
-              <br />
-              Shipping: $2.00
-              <br />
-              Total: $17.89
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </CardGroup>
-    </div>
+          <CardGroup className="order-confirmation-summary">
+            <Card>
+              <Card.Body>
+                <Card.Title>Order List: </Card.Title>
+                <ListGroup
+                  variant="flush"
+                  className="order-confirmation-scrollable-div"
+                >
+                  {cartItems.map((item) => (
+                    <ListGroup.Item key={item.id}>
+                      <Row>
+                        <Col md={2}>
+                          <Link to={`/products/${item.id}`}>
+                            <Image
+                              src={item.imageUrl}
+                              alt={item.name}
+                              fluid
+                              rounded
+                            />
+                          </Link>
+                        </Col>
+                        <Col
+                          md={2}
+                          id="cart-name"
+                          style={{ textAlign: "center" }}
+                        >
+                          <Link to={`/products/${item.id}`} id="item-name">
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col
+                          md={4}
+                          id="cart-price"
+                          style={{ textAlign: "center" }}
+                        >
+                          ${parseFloat(item.pricePrePound).toFixed(2)} X{" "}
+                          {item.qty} = $
+                          {parseFloat(item.pricePrePound * item.qty).toFixed(2)}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Body>
+                <Card.Title>Order Summary </Card.Title>
+                <Card.Text className="order-confirmation-indent">
+                  <strong>Subtotal</strong>: ${subTotal}
+                  <br />
+                  Tax: ${tax}
+                  <br />
+                  Total: ${orders.total}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </div>
+      )}
+    </>
   );
 }
--- scrumptious-coffee/src/pages/paymentScreen/StripePaymentForm.jsx
+++ scrumptious-coffee/src/pages/paymentScreen/StripePaymentForm.jsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const StripePaymentForm = (props) => {
-  const [orderInformation, setOrderInformation] = useState();
   const stripeKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
 
   const navigate = useNavigate();
 
   function sendOrder() {
-    // const url =
-    //   "http://scrumptious-env-2.eba-ixgv7adq.us-east-1.elasticbeanstalk.com/order";
-    const url = "http://localhost:8080/order";
+    const url =
+      "http://scrumptious-env-2.eba-ixgv7adq.us-east-1.elasticbeanstalk.com/order";
     axios
       .post(url, {
         user_id: 1,
         total: parseFloat(props.amount).toFixed(2),
       })
-      // .then((response) => console.log(response))
       .catch((err) => console.log(err));
   }
 
@@ -34,25 +30,28 @@ const StripePaymentForm = (props) => {
         if (response.status === 200) {
           // Clear cart
           sendOrder();
-          const orderInfo = {
-            shipName: shipping.billing_name,
-            billName: shipping.shipping_name,
-            billAddress: shipping.billing_address_line1,
-            shipAddress: shipping.shipping_address_line1,
-            email: token.email,
-            last4: token.card.last4,
-            total: props.amount,
-          };
-          setOrderInformation(orderInfo);
-          console.log({ token, shipping });
-          window.localStorage.setItem("order", JSON.stringify(orderInfo));
+          if (window.localStorage.getItem("order") !== null) {
+            console.log("nope");
+          } else {
+            window.localStorage.setItem(
+              "order",
+              JSON.stringify({
+                id: token.id,
+                shipName: shipping.billing_name,
+                billName: shipping.shipping_name,
+                billAddress: shipping.billing_address_line1,
+                shipAddress: shipping.shipping_address_line1,
+                email: token.email,
+                last4: token.card.last4,
+                total: props.amount,
+              })
+            );
+          }
           navigate("/order-confirmation");
         }
       })
       .catch((err) => console.log(err));
   }
-
-  console.log(orderInformation);
 
   return (
     <>
